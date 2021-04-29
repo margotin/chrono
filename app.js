d551c7eada7b27fbce588965f28e72148fc764c1
margotin <57538053+margotin@users.noreply.github.com>
2021-04-29 15:17:57 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+function getChrono(date, text) {
+  const now = new Date().getTime();
+  const countdownDate = new Date(date).getTime();
+  const distanceBase = countdownDate - now;
+
+  const daysInMilli = 1000 * 60 * 60 * 24;
+  const hoursInMilli = 1000 * 60 * 60;
+  const minutesInMilli = 1000 * 60;
+  const secondsInMilli = 1000;
+
+  const days = Math.floor(distanceBase / daysInMilli);
+  const hours = Math.floor((distanceBase % daysInMilli) / hoursInMilli);
+  const minutes = Math.floor((distanceBase % hoursInMilli) / minutesInMilli);
+  const seconds = Math.floor((distanceBase % minutesInMilli) / secondsInMilli);
+
+  text.innerText = `${days}j ${hours}h ${minutes}m ${seconds}s`;
+
+  if (distanceBase <= 0) {
+    clearInterval(countdownInterval);
+  }
+}
+
+const text = document.querySelector("h1");
+const date = "May 1, 2021";
+
+const countdownInterval = setInterval(getChrono, 1000, date, text);
